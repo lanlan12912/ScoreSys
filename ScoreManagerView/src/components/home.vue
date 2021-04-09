742f48cce0fcd29f41ee722a9c0fa5b8968072a9
yelanlan <yelanlan@sunline.cn>
2021-04-09 09:37:46 +0800

    角色管理界面开发

---
+++ ScoreManagerView/src/components/home.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="layout">
+    <Layout>
+      <Header :style="{padding: 0}" class="header">
+        <Row>
+          <Col span="3" class="title"><p>综合测评管理系统</p></Col>
+          <Col span="1">
+            <Icon @click.native="collapsedSider" :class="rotateIcon" class="colseIcon"  type="md-menu" size="24"></Icon>
+          </Col>
+          <Col span="4" class="rightHead">
+            <Avatar icon="ios-person" size="large" :src="headAvatar" class="headAvatar" />
+            <span class="name">{{currentUser.userName}}</span>
+            <span  @click="loginOut" class="extSpan"><Icon type="md-power" />退出</span>
+          </Col>
+        </Row>
+      </Header>
+      <Layout style="top: 64px;bottom: 0px;left: 0px;right: 0px;">
+        <Sider ref="side1" hide-trigger collapsible :collapsed-width="100" v-model="isCollapsed" class="sider">
+          <Menu theme="light" mode="vertical" width="auto" :class="menuitemClasses" @on-select="changeMenu" :active-name="$route.path">
+             <template v-for="(item,index) in menuList">
+              <Submenu v-if="item.childPages&&item.childPages.length>0" :name="item.menuPath||item.menuName">
+                  <template slot="title">
+                      <Icon :type="item.menuIcon" /><span v-if="!isCollapsed">{{item.menuName}}</span>
+                  </template>
+                  <template v-for="(child,index) in item.childPages">
+                      <MenuItem :name="child.menuPath||child.menuName"><Icon :type="child.menuIcon" /><span v-if="!isCollapsed">{{child.menuName}}</span></MenuItem>
+                  </template>
+              </Submenu>
+              <MenuItem  v-else :name="item.menuPath" ><Icon :type="item.menuIcon" /><span v-if="!isCollapsed">{{item.menuName}}</span></MenuItem>
+          </template>
+          </Menu>
+        </Sider>
+        <Content class="Content">
+          <Row class="bread">
+              <TagPage></TagPage>
+          </Row>
+          <div id="contentView" class="content-view">
+            <router-view></router-view>
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
+  </div>
+</template>
+<script>
+import '../less/home.less'
+import TagPage from './common/tag-page.vue'
+export default {
+  name:"Home",
+  components:{
+    TagPage
+  },
+  data(){
+    return{
+      isCollapsed:false,
+      headAvatar: require("../images/loginbackgroud.jpg"),
+      currentUser:'',
+      menuList:[],
+    }
+  },
+  watch:{
+    $route:{
+      handler(newval,oldval){
+        if(newval.path == '/home') return;
+        this.$store.commit("addTag",this.$router.currentRoute);
+      }   
+    }
+  },
+  mounted(){
+    this.getCurrentUser();
+    this.quUserMenuList()
+  },
+  computed: {
+    pageTagsList() {
+      return this.$store.state.pageOpenedList; // 打开的页面的页面对象
+    },
+    rotateIcon () {
+      return [
+        'menu-icon',
+        this.isCollapsed ? 'rotate-icon' : ''
+      ];
+    },
+    menuitemClasses () {
+      return [
+        // 'menu-item',
+        this.isCollapsed ? 'collapsed-menu' : ''
+      ]
+    }
+  },
+  methods:{
+    changeMenu(path){
+      this.$router.push({path:path})
+    },
+    loginOut(){
+      this.$store.commit("delToken")
+      this.$router.push({path:"/login"})
+      this.$store.commit("removeAllTags")
+    },
+    collapsedSider () {
+      this.$refs.side1.toggleCollapse();
+    },
+    saveUserInfo(){
+      let user={
+        userName:"张祥雨",
+        userRole:"STUDENT",
+        userTeleno:"1254487978",
+        userDesc:"本科生"
+      };
+      this.$http.post("/saveUser",user).then(
+        res =>{
+          if(res.success){
+            this.$Message.success(res.msg);
+          }else{
+            this.$Message.error(res.msg);
+          }
+        }
+      )
+    },
+    getCurrentUser(){
+      this.$http.post("/getCurrentUser").then(
+        res =>{
+          this.currentUser = res;
+          this.$store.commit("setUser",res);
+        },
+        err =>{
+          this.$Message.error("请求失败")
+        }
+      )
+    },
+    quUserMenuList(){
+      this.$http.post("/quMemuList").then(
+        res => {
+          if(res.success){
+            this.menuList = res.data;
+          }
+        }
+      ).catch(err => {this.$Message.error("请求异常")})
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
---
+++ ScoreManagerView/src/store/store.js
@@ -0,0 +1,58 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+Vue.use(Vuex);
+const store = new Vuex.Store({
+    state: {
+        // 存储token
+        token: localStorage.getItem('token') ? localStorage.getItem('token') : '',
+        user:{},
+        tagList:[],
+    },
+    mutations: {
+        // 修改token，并将token存入localStorage
+        setToken (state,token) {
+            state.token = token;
+            localStorage.setItem("token",token);     //存储token
+        },
+        delToken (state) {
+            state.token = '';
+            localStorage.removeItem("token");    //删除token
+        },
+        setUser(state,user){
+            state.user = user;
+        },
+        addTag(state,tag){
+          //先判断数组当中是否已经存在该tag
+          const isResult = state.tagList.some(item => {
+              return item.path == tag.path;
+          });
+          if (isResult) return;
+          state.tagList.push(tag);
+        },
+        delTag(state,tag){
+          for (let i = 0; i <= state.tagList.length; i++) {
+            if (state.tagList[i] == tag) {
+                state.tagList.splice(i, 1);
+                break;
+            }
+          }
+        },
+        removeAllTags(state,tag){
+          state.tagList.splice(1,state.tagList.length)
+        }
+    },
+    getters:{
+        getUser(state){
+            return state.user;
+        },
+        getTagList(state){
+          return state.tagList;
+        }
+    }
+});
+export default {
+    install: function (Vue, options) {
+      Vue.prototype.$store = store
+    },
+    store: store
+  };
